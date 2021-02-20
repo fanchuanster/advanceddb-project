7a0228576c5170b236c57242585a38bc0362cbe8
Wen Dong <wen.dong@microfocus.com>
2021-02-20 19:34:55 +0800

    t

--- mnist_keras.py
+++ mnist_keras.py
@@ -9,4 +9,13 @@ import tensorflow_datasets as tfds
     with_info=True,
 )
 
-print(ds_train.shape)
+def normalize_img(image, label):
+  """Normalizes images: `uint8` -> `float32`."""
+  return tf.cast(image, tf.float32) / 255., label
+
+ds_train = ds_train.map(
+    normalize_img, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+ds_train = ds_train.cache()
+ds_train = ds_train.shuffle(ds_info.splits['train'].num_examples)
+ds_train = ds_train.batch(128)
+ds_train = ds_train.prefetch(tf.data.experimental.AUTOTUNE)
