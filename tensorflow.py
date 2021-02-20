63b871562c690c5398e3ae765ff9a4a74098d0b4
Wen Dong <wen.dong@microfocus.com>
2021-02-20 20:53:20 +0800

    test

---
+++ tensorflow.py
@@ -0,0 +1,15 @@
+import os
+import tensorflow as tf
+
+# Turn off TensorFlow warning messages in program output
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+# Define computational grph
+X = tf.placeholder(tf.float32, nme='X')
+Y = tf.placeholder(tf.float32, nme='Y')
+
+addition = tf.add(X, Y, name='addition')
+
+with tf.Session() as session:
+    result = session.run(addition, feed_dict={x: [1,1,2], Y:[4,2,3]})
+    print(result)
