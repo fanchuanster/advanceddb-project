a2816d4ba7c08fac64e68f7911a448d18ccf4d8f
Wen Dong <wen.dong@microfocus.com>
2021-03-02 19:25:04 +0800

    t

--- load_data.py
+++ load_data.py
@@ -2,31 +2,32 @@ from sklearn.preprocessing import MinMaxScaler
 from sklearn.utils import shuffle
 import pandas as pd
 
+def load_data():
+    datafile = "./datasets/diabetes_1.csv"
 
-datafile = "./datasets/diabetes_1.csv"
+    df = shuffle(pd.read_csv(datafile ,dtype=float))
 
-print("hi")
+    training_data = df.sample(frac=0.8)
+    X_training = training_data.drop('Outcome', axis=1).values
+    Y_training = training_data[['Outcome']].values
 
-df = shuffle(pd.read_csv(datafile ,dtype=float))
+    testing_data = df.drop(training_data.index)
+    X_testing = testing_data.drop('Outcome', axis=1).values
+    Y_testing = testing_data[['Outcome']].values
 
-training_data = df.sample(frac=0.8)
-X_training = training_data.drop('Outcome', axis=1).values
-Y_training = training_data[['Outcome']].values
+    print(len(df.index))
+    print(len(training_data.index))
+    print(len(testing_data.index))
+    print(df.columns)
 
-testing_data = df.drop(training_data.index)
-X_testing = testing_data.drop('Outcome', axis=1).values
-Y_testing = testing_data[['Outcome']].values
+    X_scaler = MinMaxScaler(feature_range=(0, 1))
+    Y_scaler = MinMaxScaler(feature_range=(0, 1))
+    X_scaled_training = X_scaler.fit_transform(X_training)
+    Y_scaled_training = Y_scaler.fit_transform(Y_training)
+
+    X_scaled_testing = X_scaler.transform(X_testing)
+    Y_scaled_testing = Y_scaler.transform(Y_testing)
+    # print(X_scaled_training)
+    return (X_scaled_training,Y_scaled_training), (X_scaled_testing, Y_scaled_testing)
 
 
-print(len(df.index))
-print(len(training_data.index))
-print(len(testing_data.index))
-
-X_scaler = MinMaxScaler(feature_range=(0, 1))
-Y_scaler = MinMaxScaler(feature_range=(0, 1))
-X_scalered_training = X_scaler.fit_transform(X_training)
-Y_scalered_training = Y_scaler.fit_transform(Y_training)
-
-X_scalered_testing = X_scaler.transform(X_testing)
-Y_scalered_testing = Y_scaler.transform(Y_testing)
-print(X_scalered_training)
